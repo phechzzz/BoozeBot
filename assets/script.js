52dc11bd0fbc7036d884a28bb02cc932226fdf08
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2023-09-28 20:25:48 -0600

    added cocktailDB test code

--- assets/script.js
+++ assets/script.js
@@ -10,14 +10,16 @@ atBarButton.addEventListener('click', function(){
 })
 
 var name = 'bloody mary';
-var apiKey = 'sadkvCxD+O9QsMM0qn+klw==UuKcm3V674QaCQWo'; 
+var apiKeyNinja = 'sadkvCxD+O9QsMM0qn+klw==UuKcm3V674QaCQWo'; 
+const apiKeyDb = "1"
+const baseURL = `https://www.thecocktaildb.com/api/json/v2/${apiKeyDb}`;
 
 var APINinjaurl = 'https://api.api-ninjas.com/v1/cocktail?name=' + encodeURIComponent(name);
 
 fetch(APINinjaurl, {
     method: 'GET',
     headers: {
-        'X-Api-Key': apiKey,
+        'X-Api-Key': apiKeyNinja,
         'Content-Type': 'application/json'
     }
 })
@@ -32,4 +34,38 @@ fetch(APINinjaurl, {
 })
 .catch(function(error) {
     console.error('Error: ', error);
+});
+
+
+//CocktailDB test code
+fetch(baseURL + "/random.php")
+.then((response) => {
+    if (!response.ok) {
+    throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+.then((data) => {
+    // Extract a random drink from the response
+    const randomDrink = data.drinks[0];
+    if (randomDrink) {
+      // Log the random drink details
+    console.log('Random Drink:');
+    console.log('Name:', randomDrink.strDrink);
+    console.log('Category:', randomDrink.strCategory);
+    console.log('Ingredients:');
+    for (let i = 1; i <= 15; i++) {
+        const ingredient = randomDrink['strIngredient' + i];
+        const measure = randomDrink['strMeasure' + i];
+        if (ingredient) {
+        console.log(`${measure} ${ingredient}`);
+        }
+    }
+    console.log('Instructions:', randomDrink.strInstructions);
+    } else {
+    console.log('No random drink found.');
+    }
+})
+.catch((error) => {
+    console.error('Error:', error);
 });
